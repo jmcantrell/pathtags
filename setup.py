2b551dc82ffaed1c47bc8fcd61a62fe55add241b
Jeremy Cantrell <jmcantrell@gmail.com>
2019-10-30 00:11:59 -0500

    Change formatting to meet python style recommendations

--- setup.py
+++ setup.py
@@ -3,30 +3,38 @@
 from setuptools import setup
 
 setup(
-        name='pathtags',
-        version='0.9.2',
-        description='File tagging system that uses the filesystem for storage.',
-        author='Jeremy Cantrell',
-        author_email='jmcantrell@gmail.com',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: End Users/Desktop',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            ],
-        install_requires=[
-            'PathUtils',
-            'ScriptUtils',
-            ],
-        entry_points={
-            'console_scripts': [
-                'pathtags=pathtags:main',
-                ]
-            },
-        py_modules=[
-            'pathtags',
-            ],
-        )
+
+    name='pathtags',
+    version='0.9.2',
+
+    description='File tagging system that uses the filesystem for storage.',
+
+    author='Jeremy Cantrell',
+    author_email='jmcantrell@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+
+    install_requires=[
+        'pathutils',
+        'scriptutils',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'pathtags=pathtags:main',
+        ]
+    },
+
+    py_modules=[
+        'pathtags',
+    ],
+
+)
